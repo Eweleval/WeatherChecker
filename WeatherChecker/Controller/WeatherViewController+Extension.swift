f78258b73224cac1e7825089fec8016a7b82a543
WellzVal <valeweleokenyi@gmail.com>
2022-06-28 20:31:23 +0100

    feat: setup iboutlet and extension file

---
+++ WeatherChecker/Controller/WeatherViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController+Extension.swift
+//  WeatherChecker
+//
+//  Created by Decagon on 28/06/2022.
+//
+
+import Foundation
+
+extension WeatherViewController: CurrentDataDelegate, ForecastDataDelegate {
+    func receiveData(_ data: CurrentWeatherModel) {
+        <#code#>
+    }
+    
+    func receiveData(_ data: ForecastWeatherModel) {
+        <#code#>
+    }
+    
+    
+}
---
+++ WeatherChecker/Controller/WeatherViewController.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherViewController.swift
+//  WeatherChecker
+//
+//  Created by Decagon on 28/06/2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    //MARK: - Properities
+    let forecastModel = ForecastWeatherViewModel()
+    let currentModel = CurrentWeatherViewModel()
+    var forecastData: ForecastWeatherModel?
+    var currentData: CurrentWeatherModel?
+
+    // MARK: - CurrentWeather Outlets
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var condition: UILabel!
+    @IBOutlet weak var minimumTemp: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var maximumTemp: UILabel!
+
+    // MARK: - Forecast Outlets
+    @IBOutlet weak var dayOne: UILabel!
+    @IBOutlet weak var dayTwo: UILabel!
+    @IBOutlet weak var dayThree: UILabel!
+    @IBOutlet weak var dayFour: UILabel!
+    @IBOutlet weak var dayFive: UILabel!
+    @IBOutlet weak var dayOneTemp: UILabel!
+    @IBOutlet weak var dayTwoTemp: UILabel!
+    @IBOutlet weak var dayThreeTemp: UILabel!
+    @IBOutlet weak var dayFourTemp: UILabel!
+    @IBOutlet weak var dayFiveTemp: UILabel!
+    @IBOutlet weak var dayOneCondition: UIImageView!
+    @IBOutlet weak var dayTwoCondition: UIImageView!
+    @IBOutlet weak var dayThreeCondition: UIImageView!
+    @IBOutlet weak var dayFourCondition: UIImageView!
+    @IBOutlet weak var dayFiveCondition: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        currentModel.delegate = self
+        currentModel.receiveData()
+        forecastModel.delegate = self
+        forecastModel.receiveData()
+    }
+
+
+}
+
